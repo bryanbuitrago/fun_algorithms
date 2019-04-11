a0697ea1b5ddf51bac6c54c02c9d44eca02ea370
bryan <bryanbuitrago86@gmail.com>
2019-04-10 20:15:56 -0400

    Algorithm solved

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -3,11 +3,27 @@
 // The fibonacci series is an ordering of numbers where
 // each number is the sum of the preceeding two.
 // For example, the sequence
-//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+ // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Recursive solution
+function fib(n) {
+  if(n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+// function fib(n) {
+//   let result = [0,1];
+//   for (let num = 2; num <= n; num++) {
+//     const a = result[num - 1];
+//     const b = result[num - 2];
+//     result.push(a + b);
+//   }
+//   return result[result.length -1];
+// }
 
 module.exports = fib;
